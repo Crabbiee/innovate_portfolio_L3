304c63ff6b31e7a1b7f3ed4753e2815a830dac0e
Abbie <abbiedavs@gmail.com>
2022-06-27 17:05:33 +0100

    Added python notes

---
+++ python_notes/recap.py
@@ -0,0 +1,13 @@
+greeting = "Hello world"
+print(f"This is a variable > {greeting}")
+
+print(greeting.lower())
+print(f"There are {len(greeting) - greeting.count(' ')} characters in \"{greeting}\"")
+
+print("This is a string for displaying characters")
+print("1234") #this is a string
+print(1234+1) #this is an integer/whole number
+print(12.34) #this is a float
+print(True) #one of the boolean data values
+print(False) #one of the boolenans
+print(None) # blank/null/no data
